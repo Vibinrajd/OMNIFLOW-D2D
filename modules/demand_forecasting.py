183d72f708f4174536c0db9e7bd22ac274271ca9
Dhanapriya226 <dhanapriyad2004@gmail.com>
2026-01-19 19:40:54 +0530

    Update demand_forecasting.py

--- modules/demand_forecasting.py
+++ modules/demand_forecasting.py
@@ -282,3 +282,21 @@ def demand_forecasting_page():
     )
 
     st.success("✅ Demand Forecasting Completed Successfully")
+
+    # SAVE FULL FORECAST FOR DOWNSTREAM MODULES
+    FULL_FORECAST_PATH = os.path.join("data", "forecast_demand.csv")
+
+    full_output = data[
+      [
+        "date",
+        "store_id",
+        "product_id",
+        "daily_sales",
+        "forecast",
+        "lower_ci",
+        "upper_ci"
+      ]
+   ]
+
+   full_output.to_csv(FULL_FORECAST_PATH, index=False)
+
